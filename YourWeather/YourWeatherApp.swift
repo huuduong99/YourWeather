f9325df04e3548559b910ac902533acfc6b984c4
duongnh <huuduong3105@gmail.com>
2023-02-03 09:06:27 +0700

    Initial Commit

---
+++ YourWeather/YourWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourWeatherApp.swift
+//  YourWeather
+//
+//  Created by duongnh on 03/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct YourWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
